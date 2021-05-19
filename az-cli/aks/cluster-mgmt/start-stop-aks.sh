02c4e504978e0d379cb7cf5127c35c147cf24d26
gits <gits@evangelion.snotfcp.com>
2021-05-19 09:28:29 +0100

    Add improvements in the cluster status output

--- az-cli/aks/cluster-mgmt/start-stop-aks.sh
+++ az-cli/aks/cluster-mgmt/start-stop-aks.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 showHelp() {
 cat << EOF  
 Usage: 
@@ -48,6 +47,102 @@ esac
 shift
 done
 
+
+function printTable()
+{
+    local -r delimiter="${1}"
+    local -r data="$(removeEmptyLines "${2}")"
+
+    if [[ "${delimiter}" != '' && "$(isEmptyString "${data}")" = 'false' ]]
+    then
+        local -r numberOfLines="$(wc -l <<< "${data}")"
+
+        if [[ "${numberOfLines}" -gt '0' ]]
+        then
+            local table=''
+            local i=1
+
+            for ((i = 1; i <= "${numberOfLines}"; i = i + 1))
+            do
+                local line=''
+                line="$(sed "${i}q;d" <<< "${data}")"
+
+                local numberOfColumns='0'
+                numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
+
+                # Add Line Delimiter
+
+                if [[ "${i}" -eq '1' ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+
+                # Add Header Or Body
+
+                table="${table}\n"
+
+                local j=1
+
+                for ((j = 1; j <= "${numberOfColumns}"; j = j + 1))
+                do
+                    table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
+                done
+
+                table="${table}#|\n"
+
+                # Add Line Delimiter
+
+                if [[ "${i}" -eq '1' ]] || [[ "${numberOfLines}" -gt '1' && "${i}" -eq "${numberOfLines}" ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+            done
+
+            if [[ "$(isEmptyString "${table}")" = 'false' ]]
+            then
+                echo -e "${table}" | column -s '#' -t | awk '/^\+/{gsub(" ", "-", $0)}1'
+            fi
+        fi
+    fi
+}
+
+function removeEmptyLines()
+{
+    local -r content="${1}"
+
+    echo -e "${content}" | sed '/^\s*$/d'
+}
+
+function repeatString()
+{
+    local -r string="${1}"
+    local -r numberToRepeat="${2}"
+
+    if [[ "${string}" != '' && "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]
+    then
+        local -r result="$(printf "%${numberToRepeat}s")"
+        echo -e "${result// /${string}}"
+    fi
+}
+
+function isEmptyString()
+{
+    local -r string="${1}"
+
+    if [[ "$(trimString "${string}")" = '' ]]
+    then
+        echo 'true' && return 0
+    fi
+
+    echo 'false' && return 1
+}
+
+function trimString()
+{
+    local -r string="${1}"
+
+    sed 's,^[[:blank:]]*,,' <<< "${string}" | sed 's,[[:blank:]]*$,,'
+}
 
 
 OPER_TYPE=(start stop status)
@@ -103,13 +198,26 @@ elif [[ "$AKS_OPERATION_TYPE" == "status" ]]; then
   AKS_ARRAY=($(az aks list \
      --output json | jq -r ".[] | [ .name, .resourceGroup ] | @csv"))
   
+  declare -a AKS_STATUS_ARRAY
+  AKS_STATUS_ARRAY=("ClusterName,RG,Status")
+
   for akscl in "${AKS_ARRAY[@]}"; do
      AKS_CL_ARRAY=($(echo $akscl | tr -d '"' |tr "," "\n"))
   
      ## Execute Show Status
      AKS_STATUS=$(az aks show --name ${AKS_CL_ARRAY[0]} --resource-group ${AKS_CL_ARRAY[1]} -o json | jq -r '.powerState.code')
-     echo "AKS Cluster ${AKS_CL_ARRAY[0]} in RG: ${AKS_CL_ARRAY[1]} is: " $AKS_STATUS
+     #echo "AKS Cluster ${AKS_CL_ARRAY[0]} in RG: ${AKS_CL_ARRAY[1]} is: " $AKS_STATUS
+     
+     AKS_STATUS_ARRAY+=("${AKS_CL_ARRAY[0]},${AKS_CL_ARRAY[1]},$AKS_STATUS")
   done
+
+  clear
+  for i in "${AKS_STATUS_ARRAY[@]}"
+  do
+     AKS_STATUS_LIST+=$i"\n"
+  done
+
+  printTable ',' $AKS_STATUS_LIST
 
 elif [[ "$AKS_OPERATION_SCOPE" == "one" ]]; then
   ## Get AKS
@@ -149,7 +257,6 @@ elif [[ "$AKS_OPERATION_SCOPE" == "one" ]]; then
   ## Execute Operation
   echo "$AKS_OPERATION_TYPE cluster ${TMP_AKS_CHOICE_ARRAY[0]}"
   az aks $AKS_OPERATION_TYPE --name ${TMP_AKS_CHOICE_ARRAY[0]} --resource-group ${TMP_AKS_CHOICE_ARRAY[1]} --debug
-
 fi
 
 
