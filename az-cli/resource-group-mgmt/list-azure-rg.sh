37303ba5dffeb696c45858c43717083940f271f9
gits <gits@evangelion.snotfcp.com>
2021-09-19 12:43:16 +0100

    Improve output layout

--- az-cli/resource-group-mgmt/list-azure-rg.sh
+++ az-cli/resource-group-mgmt/list-azure-rg.sh
@@ -1,5 +1,125 @@
 #!/bin/bash
 
 
-echo "List Azure RG"
-az group list -o json | jq -r '(["Location","AKSClusterName","Status"] | (., map(length*"-"))),(.[] | [ .location, .name ,.properties.provisioningState ]) | @tsv' | column -t
+
+function printTable()
+{
+    local -r delimiter="${1}"
+    local -r data="$(removeEmptyLines "${2}")"
+
+    if [[ "${delimiter}" != '' && "$(isEmptyString "${data}")" = 'false' ]]
+    then
+        local -r numberOfLines="$(wc -l <<< "${data}")"
+
+        if [[ "${numberOfLines}" -gt '0' ]]
+        then
+            local table=''
+            local i=1
+
+            for ((i = 1; i <= "${numberOfLines}"; i = i + 1))
+            do
+                local line=''
+                line="$(sed "${i}q;d" <<< "${data}")"
+
+                local numberOfColumns='0'
+                numberOfColumns="$(awk -F "${delimiter}" '{print NF}' <<< "${line}")"
+
+                # Add Line Delimiter
+
+                if [[ "${i}" -eq '1' ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+
+                # Add Header Or Body
+
+                table="${table}\n"
+
+                local j=1
+
+                for ((j = 1; j <= "${numberOfColumns}"; j = j + 1))
+                do
+                    table="${table}$(printf '#| %s' "$(cut -d "${delimiter}" -f "${j}" <<< "${line}")")"
+                done
+
+                table="${table}#|\n"
+
+                # Add Line Delimiter
+
+                if [[ "${i}" -eq '1' ]] || [[ "${numberOfLines}" -gt '1' && "${i}" -eq "${numberOfLines}" ]]
+                then
+                    table="${table}$(printf '%s#+' "$(repeatString '#+' "${numberOfColumns}")")"
+                fi
+            done
+
+            if [[ "$(isEmptyString "${table}")" = 'false' ]]
+            then
+                echo -e "${table}" | column -s '#' -t | sed 's/^  //' | awk '/^\+/{gsub(" ", "-", $0)}1'
+            fi
+        fi
+    fi
+}
+
+
+function removeEmptyLines()
+{
+    local -r content="${1}"
+
+    echo -e "${content}" | sed '/^\s*$/d'
+}
+
+
+function repeatString()
+{
+    local -r string="${1}"
+    local -r numberToRepeat="${2}"
+
+    if [[ "${string}" != '' && "${numberToRepeat}" =~ ^[1-9][0-9]*$ ]]
+    then
+        local -r result="$(printf "%${numberToRepeat}s")"
+        echo -e "${result// /${string}}"
+    fi
+}
+
+
+function isEmptyString()
+{
+    local -r string="${1}"
+
+    if [[ "$(trimString "${string}")" = '' ]]
+    then
+        echo 'true' && return 0
+    fi
+
+    echo 'false' && return 1
+}
+
+
+function trimString()
+{
+    local -r string="${1}"
+
+    sed 's,^[[:blank:]]*,,' <<< "${string}" | sed 's,[[:blank:]]*$,,'
+}
+
+
+declare -a RG_ARRAY
+RG_ARRAY=($(az group list -o json | jq -r "(.[] | select( .name | startswith(\"rg-\")) | [ .location, .name , .properties.provisioningState ]) | @csv"))
+
+declare -a RG_STATUS_ARRAY
+RG_STATUS_ARRAY=("Location,RGName,Status")
+
+declare -a RG_CL_ARRAY
+
+for rg in "${RG_ARRAY[@]}"; 
+do
+   RG_CL_ARRAY=($(echo $rg | tr -d '"' | tr "," "\n"))
+   RG_STATUS_ARRAY+=("${RG_CL_ARRAY[0]},${RG_CL_ARRAY[1]},${RG_CL_ARRAY[2]}")
+done
+
+for i in "${RG_STATUS_ARRAY[@]}"
+do
+   RG_STATUS_LIST+=$i"\n"
+done
+
+printTable ',' $RG_STATUS_LIST
