cd79cef9df4554353358865db4463a24b037f601
gits <gits@evangelion.snotfcp.com>
2022-02-10 08:28:16 +0000

    Beta - Add IngCrtl for linux and windows pods - nginx SSI enabled

---
+++ az-cli/aks/deployments/ingress/linux-and-windows/create.sh
@@ -0,0 +1,19 @@
+##!/usr/bin/env bash
+
+# Add the ingress-nginx repository
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+
+# Use Helm to deploy an NGINX ingress controller
+helm install nginx-ingress ingress-nginx/ingress-nginx \
+    --namespace default \
+    --set controller.replicaCount=2 \
+    --set controller.nodeSelector."kubernetes\.io/os"=linux \
+    --set defaultBackend.nodeSelector."kubernetes\.io/os"=linux \
+    --set controller.admissionWebhooks.patch.nodeSelector."kubernetes\.io/os"=linux \
+    --set controller.service.externalTrafficPolicy=Local \
+    --set controller.service.annotations."service\.beta\.kubernetes\.io/azure-load-balancer-internal"="true"
+
+sleep 30
+
+kubectl apply -f ingress.yaml
+
