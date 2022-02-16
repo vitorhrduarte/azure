17d91b66553bfe72e1af7b7fd44d9dba814c04dc
gits <gits@evangelion.snotfcp.com>
2022-02-16 10:08:02 +0000

    Code improvement

--- az-cli/vm/mail-server/create.sh
+++ az-cli/vm/mail-server/create.sh
@@ -2,14 +2,14 @@
 set -e
 . ./params.sh
 
-
-if [[ $CREATE_POSTFIX -eq "0" && $CREATE_MAIL_SRV -eq "1" ]]; then
+if [[ $CREATE_POSTFIX -eq "1" && $CREATE_MAIL_SRV -eq "1" ]]; then
     echo "Create VM"
     bash create-mail-srv.sh
+    bash create-postfix.sh
     echo ""
 fi
 
-if [[ $CREATE_POSTFIX -eq "1" ||  $CREATE_MAIL_SRV -eq "0" ]]; then
+if [[ $CREATE_POSTFIX -eq "1" && $CREATE_MAIL_SRV -eq "0" ]]; then
     echo "Setup Mail Server - Postfix"
     bash create-postfix.sh
 fi
