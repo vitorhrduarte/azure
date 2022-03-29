25bb36227688779bf7ced47d3c6d87a7f260aeae
gits <gits@evangelion.snotfcp.com>
2022-03-29 15:48:03 +0100

    Add input parameter for yaml file

--- az-cli/vm/dns-server/core-dns-config-maps/apply.sh
+++ az-cli/vm/dns-server/core-dns-config-maps/apply.sh
@@ -1,5 +1,53 @@
 ##!/usr/bin/env bash
 
+
+showHelp() {
+cat << EOF  
+Usage: 
+
+bash apply.sh --help/-h  [for help]
+bash apply.sh -f/--file <file-name>
+
+Install Pre-requisites jq and dialog
+
+-h, -help,          --help                  Display help
+
+-f, -file,          --file                  Input yaml file name
+
+EOF
+}
+
+
+
+options=$(getopt -l "help::,file:" -o "h::f:" -a -- "$@")
+
+eval set -- "$options"
+
+while true
+do
+case $1 in
+-h|--help) 
+    showHelp
+    exit 0
+    ;;
+-f|--file)
+    shift
+    YAML_FILE_NAME=$1
+    ;;
+--)
+    shift
+    break
+    exit 0
+    ;;
+esac
+shift
+done
+
+
+
+
+
+
 getCoreDnsCustomConfigMap () {
     echo "Get Current Custom CoreDNS CM"
     kubectl -n kube-system describe cm coredns-custom
@@ -11,7 +59,7 @@ getCoreDnsCustomConfigMap
 
 ## Apply CoreDNS ConfigMap
 echo "Apply CoreDNS ConfigMap"
-kubectl apply -f core-dns-cm.yaml 
+kubectl apply -f $YAML_FILE_NAME 
 
 ## Re-deploy CoreDNS pods 
 echo "Re-deploy CoreDNS pods"
