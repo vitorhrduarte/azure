2021f8294eefcc2c398d24d0abf6257089e242c0
gits <gits@evangelion.snotfcp.com>
2022-04-14 14:09:38 +0100

    Code improvement

--- az-cli/aks/http-routing/create.sh
+++ az-cli/aks/http-routing/create.sh
@@ -62,8 +62,24 @@ funcHttpRoutingStatus () {
   AKS_HR_STATUS=$(az aks show \
     --resource-group $AKS_RG_NAME \
     --name $AKS_NAME \
-    --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName \
-    --output table)
+    --output json &>/dev/null | jq -r "select ( .addonProfiles.httpApplicationRouting.enabled == \"true\") | [ .addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName ] | @tsv" | wc -l)
+  
+  if [[ "$AKS_HR_STATUS" == "0" ]];
+  then
+    echo ""
+    echo "Http Routing in not enabled in Cluster: $AKS_NAME in RG: $AKS_RG_NAME"
+    echo ""
+    echo "Exiting"
+    exit 1
+  fi
+
+  AKS_HR_STATUS=$(az aks show \
+    --resource-group $AKS_RG_NAME \
+    --name $AKS_NAME \
+    --output json  &>/dev/null | jq -r " select ( .addonProfiles.httpApplicationRouting.enabled == "true") | [ .addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName ] | @tsv")
+  echo ""
+  echo "AKS Http Routing Status"
+  echo "$AKS_HR_STATUS"
 }
 
 
@@ -92,40 +108,122 @@ funcHttpRoutingRemove () {
 }
 
 
-funcHttpRoutingSampleApp () {
+funcHttpRoutingAksDNS () {
+  ## Get AKS Cluster DNS Name
+  echo "Get AKS Cluster DNS Name"
+  AKS_SPECIFIC_DNS_ZONE=$(az aks show \
+    --resource-group $AKS_RG_NAME \
+    --name $AKS_NAME \
+    --output json  &>/dev/null | jq -r ".addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName" | wc -l)  
 
+  if [[ "$AKS_SPECIFIC_DNS_ZONE" == "0"  ]]
+  then
+    ## If AKS has not Http Routing Defined then we need to exit
+    echo "AKS has not Http Routing Defined then we need to exit"
+    echo "Exiting..."
+    exit 1
+
+  fi
+
+  ## If AKS has Http Routing enabled then...  
+  AKS_SPECIFIC_DNS_ZONE=$(az aks show \
+    --resource-group $AKS_RG_NAME \
+    --name $AKS_NAME \
+    --output json  &>/dev/null | jq -r ".addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName")   
 
 }
 
 
-if [[ "$VNET_OPERATION_TYPE" == "status"  ]]
+funcHttpRoutingSampleApp () {
+  ## Apply sample APP
+  echo "Apply sample APP"
+
+cat <<EOF > http-routing-sample.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: aks-helloworld  
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: aks-helloworld
+  template:
+    metadata:
+      labels:
+        app: aks-helloworld
+    spec:
+      containers:
+      - name: aks-helloworld
+        image: mcr.microsoft.com/azuredocs/aks-helloworld:v1
+        ports:
+        - containerPort: 80
+        env:
+        - name: TITLE
+          value: "Http Routing in AKS is Working! Yey!"
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: aks-helloworld  
+spec:
+  type: ClusterIP
+  ports:
+  - port: 80
+  selector:
+    app: aks-helloworld
+---
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+  name: aks-helloworld
+  annotations:
+    kubernetes.io/ingress.class: addon-http-application-routing
+spec:
+  rules:
+  - host: aks-helloworld.$AKS_SPECIFIC_DNS_ZONE
+    http:
+      paths:
+      - path: /
+        pathType: Prefix
+        backend:
+          service: 
+            name: aks-helloworld
+            port: 
+              number: 80
+
+EOF
+
+
+  kubectl apply -f http-routing-sample.yaml     
+
+}
+
+
+##########################
+##
+## Core Part
+##
+##########################
+
+if [[ "$HTTP_ROUTING_OPERATION_TYPE" == "enable" ]]; 
 then
-   
-   echo "Origin Details..."
-   funcShowPeerings $JS_VNET_RG $JS_VNET_NAME
-   echo ""   
-   echo "Destination Details..."
-   funcShowPeerings $DEST_VNET_RG $DEST_VNET_NAME
-elif [[ "$VNET_OPERATION_TYPE" == "peer"  ]]
+   echo "Add Http Routing To AKS"
+   funcHttpRoutingAdd
+   echo "Get AKS Dns Details"
+   funcHttpRoutingAksDNS
+   echo "Deploy Sample APP"
+   funcHttpRoutingSampleApp
+elif [[ "$HTTP_ROUTING_OPERATION_TYPE" == "disable" ]];
 then
-   funcPeerVnet  
-elif [[ "$VNET_OPERATION_TYPE" == "unpeer" ]]
+  echo "Remove Http Routing To AKS"
+  funcHttpRoutingRemove  
+elif [[ "$HTTP_ROUTING_OPERATION_TYPE" == "status" ]];
 then
-   funcUnPeerVnet
+  echo "Get AKS Http Routing Status"
+  funcHttpRoutingStatus
 else
-   echo "Invalid Option..."
-   echo "Exiting..."
-   exit
+  echo "No available option provided"
+  exit 1
 fi
-
-
-
-
-
-
-
-
-
-
-
 
