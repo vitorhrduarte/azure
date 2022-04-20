20be36d4e8671f175b19f6d6b51bb57092fe10c6
Ubuntu <gits@ljs.gtbgjbosemxetduyonuoo02fdf.ax.internal.cloudapp.net>
2022-04-20 22:08:05 +0000

    Code improvement

--- az-cli/aks/performance/linux/regularTools/cpu/go.sh
+++ az-cli/aks/performance/linux/regularTools/cpu/go.sh
@@ -3,7 +3,7 @@
 
 ## Hard code VARS
 ## Number os Seconds that we wait for each repetition of the loop
-LOOP_SLEEP_SEC="2"
+LOOP_SLEEP_SEC="5"
 OUTPUT_FILE_NAME="out.txt"
 
 ## Functions
@@ -19,8 +19,6 @@ Install Pre-requisites JQ
 
 -h, -help,          --help                  Display help
 
--t, -top,           --top                   Select the Top X Higher CPU PID consumers (integer value)
-
 -p, -port           --percentage            Percentage that higher than X (integer value) 
 
 -l, -loop,          --loop                  If we want to run once or in a endless loop  (y/n)
@@ -28,7 +26,7 @@ Install Pre-requisites JQ
 EOF
 }
 
-options=$(getopt -l "help::,top:,percentage:,loop:" -o "h::o:t:p:l:" -a -- "$@")
+options=$(getopt -l "help::,percentage:,loop:" -o "h:::p:l:" -a -- "$@")
 
 eval set -- "$options"
 
@@ -38,10 +36,6 @@ case $1 in
 -h|--help) 
     showHelp
     exit 0
-    ;;  
--t|--top)
-    shift
-    TOP_CPU=$1
     ;;  
 -p|--percentage)
     shift
@@ -60,51 +54,46 @@ esac
 shift
 done
 
-#########
-## Core #
-#########
 
-## Get All PID that are above the PERCENTAGE_CPU 
-declare -a ARR_PS
-
-IFS=$'\n'
-
-ARR_PS=($(ps --no-headers -eo pid,%cpu,comm --sort=-%cpu | awk '$2>='"$PERCENTAGE_CPU"'' | awk '{print $1}'))
-
-
-## For each of those PID, get the one that are pods 
-declare -A ARR_PS_CONT_ID
-
-j=0
-for i in "${ARR_PS[@]}"
-do
-  TEMP_POD_ID=$(pstree -als $i | grep containerd-shim | awk '{print $5}'  | uniq)
-  
-  if [[ -z $TEMP_POD_ID ]];
-  then
-    ARR_PS_CONT_ID[$j,0]="$i"
-    ARR_PS_CONT_ID[$j,1]="PID_IS_NOT_A_POD"
-  else
-    ARR_PS_CONT_ID[$j,0]="$i"
-    ARR_PS_CONT_ID[$j,1]="$TEMP_POD_ID"
-  fi
-  
-  j=$((j+1))
-done
-
-
-## For each of those pods get the details
-i=$(expr ${#ARR_PS_CONT_ID[@]} / 2)
 
 rm -rf $OUTPUT_FILE_NAME
 touch $OUTPUT_FILE_NAME
-
 LOOP_CONTROL="0"
 
+## Get All PID that are above the PERCENTAGE_CPU 
 while :;
-do
-  for ((j=0; j<$i; j++))
+  declare -a ARR_PS=()
+  IFS=$'\n'
+  
+  ARR_PS=($(ps --no-headers -eo pid,%cpu,comm --sort=-%cpu | awk '$2>='"$PERCENTAGE_CPU"'' | awk '{print $1}'))
+
+  ## For each of those PID, get the one that are pods 
+  declare -A ARR_PS_CONT_ID=()
+
+  j=0
+  for i in "${ARR_PS[@]}"
   do
+    TEMP_POD_ID=$(pstree -als $i | grep containerd-shim | awk '{print $5}'  | uniq)
+  
+    if [[ -z $TEMP_POD_ID ]];
+    then
+      ARR_PS_CONT_ID[$j,0]="$i"
+      ARR_PS_CONT_ID[$j,1]="PID_IS_NOT_A_POD"
+    else
+      ARR_PS_CONT_ID[$j,0]="$i"
+      ARR_PS_CONT_ID[$j,1]="$TEMP_POD_ID"
+    fi
+  
+    j=$((j+1))
+  done
+
+  ## For each of those pods get the details
+  i=$(expr ${#ARR_PS_CONT_ID[@]} / 2)
+  j=0
+  
+  do
+    for ((j=0; j<$i; j++))
+    do
      if [[ "${ARR_PS_CONT_ID[$j,1]}" != "PID_IS_NOT_A_POD" ]];
      then
        if [[ "$LOOP_CONTROL" == "0" ]];
@@ -115,12 +104,15 @@ do
        
        crictl pods --id "${ARR_PS_CONT_ID[$j,1]}"  | awk 'NR>1 {print $1" "$2" "$3" "$4" "$5" "$6" "$7}'  | ts '%Y-%m-%d %H:%M:%S' | tee -a $OUTPUT_FILE_NAME
      fi
-  done
+    done
 
-  if [[ "$DO_LOOP" != "y" ]];
-  then
+    if [[ "$DO_LOOP" != "y" ]];
+    then
      exit 1
-  fi
-
-  sleep $LOOP_SLEEP_SEC
+    fi
+  
+    sleep $LOOP_SLEEP_SEC
 done
+
+
+
