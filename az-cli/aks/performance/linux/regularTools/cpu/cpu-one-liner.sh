daab0e810989e072cc1039d7f13050d1c82a983f
Ubuntu <gits@ljs.gtbgjbosemxetduyonuoo02fdf.ax.internal.cloudapp.net>
2022-04-21 08:43:47 +0000

    Init commit

---
+++ az-cli/aks/performance/linux/regularTools/cpu/cpu-one-liner.sh
@@ -0,0 +1,1 @@
+ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%cpu | awk '$4>50' | awk '{print $1}' | xargs -I '{}' bash -c "pstree -als {}" | grep containerd-shim | awk '{print $5}' | uniq | xargs -I '{}' bash -c "crictl ps --no-trunc -p {} && echo "" && crictl pods --id {}"
