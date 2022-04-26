5ba39e62cddb7fe8dea9e36e3dd94e51ac6f5649
Ubuntu <gits@ljs.gtbgjbosemxetduyonuoo02fdf.ax.internal.cloudapp.net>
2022-04-26 13:04:23 +0000

    Add one liner

--- az-cli/aks/performance/linux/regularTools/memory/mem-one-liner.sh
+++ az-cli/aks/performance/linux/regularTools/memory/mem-one-liner.sh
@@ -1,1 +1,1 @@
-ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%cpu | awk '$3>50' | awk '{print $1}' | xargs -I '{}' bash -c "pstree -als {}" | grep containerd-shim | awk '{print $5}' | uniq | xargs -I '{}' bash -c "crictl ps --no-trunc -p {} && echo "" && crictl pods --id {}"
+ps -eo pid,ppid,%mem,%cpu,cmd --sort=-%mem | awk '$3>50' | awk '{print $1}' | xargs -I '{}' bash -c "pstree -als {}" | grep containerd-shim | awk '{print $5}' | uniq | xargs -I '{}' bash -c "crictl ps --no-trunc -p {} && echo "" && crictl pods --id {}"
