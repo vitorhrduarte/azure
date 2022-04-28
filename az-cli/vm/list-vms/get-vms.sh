365444c8f2f6ab5ab15205daf023fcc2c8743ca2
gits <gits@evangelion.snotfcp.com>
2022-04-28 08:35:22 +0100

    Remove List All VMs

--- az-cli/vm/list-vms/get-vms.sh
+++ az-cli/vm/list-vms/get-vms.sh
@@ -2,10 +2,11 @@
 
 echo "Not running VMs"
 az vm list -d --query "[?powerState!='VM running']" -o table
-
+echo ""
+echo ""
 echo "Running VMs"
 az vm list -d --query "[?powerState=='VM running']" -o table
-
-
-echo "All VMs"
-az vm list -d -o table
+#echo ""
+#echo ""
+#echo "All VMs"
+#az vm list -d -o table
