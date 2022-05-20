f4f497b068b752c9a757d1cc52b20f6e27c85358
gits <gits@evangelion.snotfcp.com>
2022-05-20 13:44:43 +0100

    Init commit Add Az File Manually

---
+++ az-cli/aks/pv-pvc/az-file/manual/go.sh
@@ -0,0 +1,131 @@
+# Change these four parameters as needed for your own environment
+STR_ID=$RANDOM
+
+
+AKS_PERS_STORAGE_ACCOUNT_NAME=str-acc-$STR_ID
+AKS_PERS_RESOURCE_GROUP=rg-str-acc-$STR_ID
+AKS_PERS_LOCATION=eastus
+AKS_PERS_SHARE_NAME=aks-share-str-acc-$STR_ID
+
+
+
+# Create a resource group
+az group create \
+  --name $AKS_PERS_RESOURCE_GROUP \
+  --location $AKS_PERS_LOCATION
+
+# Create a storage account
+az storage account create \
+  --name $AKS_PERS_STORAGE_ACCOUNT_NAME \
+  --resource-group $AKS_PERS_RESOURCE_GROUP \
+  --location $AKS_PERS_LOCATION \
+  --sku Standard_LRS
+
+# Export the connection string as an environment variable, this is used when creating the Azure file share
+export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string \
+  -n $AKS_PERS_STORAGE_ACCOUNT_NAME \
+  -g $AKS_PERS_RESOURCE_GROUP \
+  -o tsv)
+
+# Create the file share
+az storage share create -n $AKS_PERS_SHARE_NAME --connection-string $AZURE_STORAGE_CONNECTION_STRING
+
+# Get storage account key
+STORAGE_KEY=$(az storage account keys list --resource-group $AKS_PERS_RESOURCE_GROUP --account-name $AKS_PERS_STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
+
+# Echo storage account name and key
+echo Storage account name: $AKS_PERS_STORAGE_ACCOUNT_NAME
+echo Storage account key: $STORAGE_KEY
+
+
+
+kubectl create secret generic azure-secret --from-literal=azurestorageaccountname=$AKS_PERS_STORAGE_ACCOUNT_NAME --from-literal=azurestorageaccountkey=$STORAGE_KEY
+
+## Mount file share as an inline volume
+
+apiVersion: v1
+kind: Pod
+metadata:
+  name: mypod
+spec:
+  containers:
+  - image: mcr.microsoft.com/oss/nginx/nginx:1.15.5-alpine
+    name: mypod
+    resources:
+      requests:
+        cpu: 100m
+        memory: 128Mi
+      limits:
+        cpu: 250m
+        memory: 256Mi
+    volumeMounts:
+      - name: azure
+        mountPath: /mnt/azure
+  volumes:
+  - name: azure
+    csi:
+      driver: file.csi.azure.com
+      volumeAttributes:
+        secretName: azure-secret  # required
+        shareName: aksshare  # required
+        mountOptions: "dir_mode=0777,file_mode=0777,cache=strict,actimeo=30"  # optional
+
+
+kubectl apply -f azure-files-pod.yaml
+
+
+
+
+# Mount file share as a persistent volume
+
+apiVersion: v1
+kind: PersistentVolume
+metadata:
+  name: azurefile
+spec:
+  capacity:
+    storage: 5Gi
+  accessModes:
+    - ReadWriteMany
+  persistentVolumeReclaimPolicy: Retain
+  storageClassName: azurefile-csi
+  csi:
+    driver: file.csi.azure.com
+    readOnly: false
+    volumeHandle: unique-volumeid  # make sure this volumeid is unique in the cluster
+    volumeAttributes:
+      resourceGroup: EXISTING_RESOURCE_GROUP_NAME  # optional, only set this when storage account is not in the same resource group as agent node
+      shareName: aksshare
+    nodeStageSecretRef:
+      name: azure-secret
+      namespace: default
+  mountOptions:
+    - dir_mode=0777
+    - file_mode=0777
+    - uid=0
+    - gid=0
+    - mfsymlinks
+    - cache=strict
+    - nosharesock
+    - nobrl
+
+
+
+apiVersion: v1
+kind: PersistentVolumeClaim
+metadata:
+  name: azurefile
+spec:
+  accessModes:
+    - ReadWriteMany
+  storageClassName: azurefile-csi
+  volumeName: azurefile
+  resources:
+    requests:
+      storage: 5Gi
+
+
+kubectl apply -f azurefile-mount-options-pv.yaml
+kubectl apply -f azurefile-mount-options-pvc.yaml
+
+
