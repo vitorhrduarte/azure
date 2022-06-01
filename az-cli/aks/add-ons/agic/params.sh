9143206aa84f649d8b97f259e44d3bc531f05889
gits <gits@evangelion.snotfcp.com>
2022-06-01 08:38:39 +0100

    Add more options for the add-on setup

--- az-cli/aks/add-ons/agic/params.sh
+++ az-cli/aks/add-ons/agic/params.sh
@@ -1,13 +1,16 @@
 ## App Gateway Settings
 APPGTW_ID=""
-APPGTW_NAME="eagles"              ## Mandatory
-APPGTW_SNET_CIDR=""               ## Mandatory
+APPGTW_NAME="eagles"                         ## Mandatory
+APPGTW_SNET_CIDR="10.5.2.0/24"               ## Mandatory
 APPGTW_SNET_ID=""
 APPGTW_WATCH_NAMESPACE=""
+APPTGTW_SNET_NAME="snet-eagles"
+
 
 ## AKS Settings
 AKS_NAME="aks-agic"
 AKS_RG_NAME="rg-$AKS_NAME"
+AKS_VNET_NAME="vnet-$AKS_NAME"
 
 ## Auth
 AUTH_TYPE="aad"                    ## SP(sp) or AAD(aad) Pod Identity
