9cfd56cbefa74ef465f4e14426d3f40cc44a5ed1
gits <gits@evangelion.snotfcp.com>
2022-08-21 10:51:17 +0100

    Adjust code to remove kubenet azure policy

--- az-cli/aks/kubenet/just-aks/create.sh
+++ az-cli/aks/kubenet/just-aks/create.sh
@@ -38,39 +38,7 @@ AKS_SNET_ID=$(az network vnet subnet show \
 
 
 echo "Creating AKS Cluster"
-if [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 1 && $HAS_MANAGED_IDENTITY -eq 1 && $HAS_NETWORK_POLICY -eq 1 ]]; then
-  echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-  echo "Creating AKS with Monitor Enabled, AutoScaler, Managed Idenity and Network Policy = Azure"
-  echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
-  az aks create \
-  --resource-group $AKS_RG_NAME \
-  --name $AKS_CLUSTER_NAME \
-  --node-count $AKS_SYSNP_COUNT \
-  --node-vm-size $AKS_SYSNP_NODE_SIZE \
-  --location $AKS_LOCATION \
-  --load-balancer-sku standard \
-  --vnet-subnet-id $AKS_SNET_ID \
-  --vm-set-type $AKS_NP_VM_SET_TYPE \
-  --kubernetes-version $AKS_VERSION \
-  --network-plugin $AKS_NETWORK_PLUGIN \
-  --service-cidr $AKS_CLUSTER_SRV_CIDR \
-  --dns-service-ip $AKS_CLUSTER_DNS \
-  --pod-cidr $AKS_POD_CIDR \
-  --docker-bridge-address $AKS_CLUSTER_DOCKER_BRIDGE \
-  --api-server-authorized-ip-ranges $MY_HOME_PUBLIC_IP"/32" \
-  --ssh-key-value $ADMIN_USERNAME_SSH_KEYS_PUB \
-  --admin-username $GENERIC_ADMIN_USERNAME \
-  --enable-addons monitoring \
-  --network-policy azure \
-  --enable-cluster-autoscaler \
-  --min-count 1 \
-  --max-count 3 \
-  --enable-managed-identity \
-  --nodepool-name sysnpool \
-  --nodepool-tags "env=syspool" \
-  --yes \
-  --debug 
-elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 1 && $HAS_MANAGED_IDENTITY -eq 1 && $HAS_NETWORK_POLICY -eq 0 ]]; then
+if [[ $AKS_HAS_AZURE_MONITOR -eq 1 && $AKS_HAS_AUTO_SCALER -eq 1 && $AKS_HAS_MANAGED_IDENTITY -eq 1 ]]; then
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
   echo "Creating AKS with Monitor Enabled, AutoScaler, Managed Idenity"
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -97,11 +65,12 @@ elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 1 && $HAS_MANAGED_IDENT
   --min-count 1 \
   --max-count 3 \
   --enable-managed-identity \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
   --yes \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --debug
-elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 1 && $HAS_MANAGED_IDENTITY -eq 0 && $HAS_NETWORK_POLICY -eq 0 ]]; then
+elif [[ $AKS_HAS_AZURE_MONITOR -eq 1 && $AKS_HAS_AUTO_SCALER -eq 1 && $AKS_HAS_MANAGED_IDENTITY -eq 0  ]]; then
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
   echo "Creating AKS with Monitor Enabled, AutoScaler"
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -129,10 +98,11 @@ elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 1 && $HAS_MANAGED_IDENT
   --enable-cluster-autoscaler \
   --min-count 1 \
   --max-count 3 \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --debug
-elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENTITY -eq 0 && $HAS_NETWORK_POLICY -eq 0 ]]; then
+elif [[ $AKS_HAS_AZURE_MONITOR -eq 1 && $AKS_HAS_AUTO_SCALER -eq 0 && $AKS_HAS_MANAGED_IDENTITY -eq 0  ]]; then
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
   echo "Creating AKS"
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -157,10 +127,11 @@ elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENT
   --ssh-key-value $ADMIN_USERNAME_SSH_KEYS_PUB \
   --admin-username $GENERIC_ADMIN_USERNAME \
   --enable-addons monitoring \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --debug
-elif [[ $HAS_AZURE_MONITOR -eq 0 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENTITY -eq 1 && $HAS_NETWORK_POLICY -eq 0 ]]; then
+elif [[ $AKS_HAS_AZURE_MONITOR -eq 0 && $AKS_HAS_AUTO_SCALER -eq 0 && $AKS_HAS_MANAGED_IDENTITY -eq 1  ]]; then
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
   echo "Creating AKS with Managed Identity"
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -183,11 +154,12 @@ elif [[ $HAS_AZURE_MONITOR -eq 0 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENT
   --ssh-key-value $ADMIN_USERNAME_SSH_KEYS_PUB \
   --admin-username $GENERIC_ADMIN_USERNAME \
   --enable-managed-identity \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --yes \
   --debug
-elif [[ $HAS_AZURE_MONITOR -eq 0 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENTITY -eq 1 && $HAS_NETWORK_POLICY -eq 1 ]]; then
+elif [[ $AKS_HAS_AZURE_MONITOR -eq 0 && $AKS_HAS_AUTO_SCALER -eq 0 && $AKS_HAS_MANAGED_IDENTITY -eq 1 && $AKS_HAS_NETWORK_POLICY -eq 1 ]]; then
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
   echo "Creating AKS with Managed Identityi and Network Policy = Azure" 
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -211,11 +183,12 @@ elif [[ $HAS_AZURE_MONITOR -eq 0 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENT
   --admin-username $GENERIC_ADMIN_USERNAME \
   --enable-managed-identity \
   --network-policy azure \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --yes \
   --debug
-elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENTITY -eq 0 && $HAS_NETWORK_POLICY -eq 0 ]]; then
+elif [[ $AKS_HAS_AZURE_MONITOR -eq 1 && $AKS_HAS_AUTO_SCALER -eq 0 && $AKS_HAS_MANAGED_IDENTITY -eq 0  ]]; then
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
   echo "Creating AKS with Monitor" 
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -240,8 +213,9 @@ elif [[ $HAS_AZURE_MONITOR -eq 1 && $HAS_AUTO_SCALER -eq 0 && $HAS_MANAGED_IDENT
   --ssh-key-value $ADMIN_USERNAME_SSH_KEYS_PUB \
   --admin-username $GENERIC_ADMIN_USERNAME \
   --enable-addons monitoring \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --debug
 else
   echo '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>'
@@ -267,31 +241,34 @@ else
   --api-server-authorized-ip-ranges $MY_HOME_PUBLIC_IP"/32" \
   --ssh-key-value $ADMIN_USERNAME_SSH_KEYS_PUB \
   --admin-username $GENERIC_ADMIN_USERNAME \
-  --nodepool-name sysnpool \
+  --nodepool-name sysnp01 \
   --nodepool-tags "env=syspool" \
+  --node-osdisk-size $AKS_SYSNP_DISK_SIZE \
   --debug
 fi
 
-## Logic for VMASS only
-if [[ "$AKS_NP_VM_SET_TYPE" == "AvailabilitySet" ]]; then
-  echo "Skip second Nodepool - VMAS dont have it"
-else
-  ## Add User Nodepool
-  echo 'Add Node pool type User'
-  az aks nodepool add \
-    --resource-group $AKS_RG_NAME \
-    --name usernpool \
-    --cluster-name $AKS_CLUSTER_NAME \
-    --node-osdisk-type Ephemeral \
-    --node-osdisk-size $AKS_USRNP_DISK_SIZE \
-    --kubernetes-version $AKS_VERSION \
-    --tags "env=userpool" \
-    --mode User \
-    --node-count $AKS_USRNP_COUNT \
-    --node-vm-size $AKS_USRNP_NODE_SIZE \
-    --debug
-fi
+if [[ "$AKS_HAS_2ND_NP" == "1" ]]; then
 
+  ## Logic for VMAS only
+  if [[ "$AKS_NP_VM_SET_TYPE" == "AvailabilitySet" ]]; then
+    echo "Skip second Nodepool - VMAS dont have it"
+  else
+    ## Add User Nodepool
+    echo 'Add Node pool type User'
+    az aks nodepool add \
+      --resource-group $AKS_RG_NAME \
+      --name usrnp01 \
+      --cluster-name $AKS_CLUSTER_NAME \
+      --node-osdisk-type Ephemeral \
+      --node-osdisk-size $AKS_USRNP_DISK_SIZE \
+      --kubernetes-version $AKS_VERSION \
+      --tags "env=usrnp01" \
+      --mode User \
+      --node-count $AKS_USRNP_COUNT \
+      --node-vm-size $AKS_USRNP_NODE_SIZE \
+      --debug
+  fi
+fi
 
 ### Get Credentials
 echo "Getting Cluster Credentials"
