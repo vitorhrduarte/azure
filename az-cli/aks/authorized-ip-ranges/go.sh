d52aaba7171f5e2bd67fd26b13a265dc9791d349
Ubuntu <gits@ljs.gtbgjbosemxetduyonuoo02fdf.ax.internal.cloudapp.net>
2022-08-24 15:01:45 +0000

    Simplify code

--- az-cli/aks/authorized-ip-ranges/go.sh
+++ az-cli/aks/authorized-ip-ranges/go.sh
@@ -6,7 +6,6 @@ AKS_RG_NAME="rg-$AKS_NAME"
 PIP=$(curl -s -4 ifconfig.io) 
 
 CURRENTPIP=$(az aks show --resource-group $AKS_RG_NAME --name $AKS_NAME --query apiServerAccessProfile.authorizedIpRanges | jq -r ". | @csv" | sed s/\"//g)
-#CURRENTPIP=$(echo "$CURRENTPIP" | sed s/\"//g)
 FINALLIST="$PIP/32,$CURRENTPIP"
 
 declare -A words
