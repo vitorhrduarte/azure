bcd8909671c9503a0b0ea31c4990f3ef9b92d749
Ubuntu <gits@ljs.gtbgjbosemxetduyonuoo02fdf.ax.internal.cloudapp.net>
2022-08-24 14:47:02 +0000

    Improve code

--- az-cli/aks/authorized-ip-ranges/go.sh
+++ az-cli/aks/authorized-ip-ranges/go.sh
@@ -3,39 +3,20 @@
 AKS_NAME="aks-kube01"
 AKS_RG_NAME="rg-$AKS_NAME"
 
-
 PIP=$(curl -s -4 ifconfig.io) 
 
-CURRENTPIP=$(az aks show --resource-group $AKS_RG_NAME --name $AKS_NAME --query apiServerAccessProfile.authorizedIpRanges | jq -r ". | @csv")
-
-#echo ""
-#echo "PIP: $PIP"
-#echo ""
-#echo "CURRENT IP: $CURRENTPIP"
-
-CURRENTPIP=$(echo "$CURRENTPIP" | sed s/\"//g)
-
+CURRENTPIP=$(az aks show --resource-group $AKS_RG_NAME --name $AKS_NAME --query apiServerAccessProfile.authorizedIpRanges | jq -r ". | @csv" | sed s/\"//g)
+#CURRENTPIP=$(echo "$CURRENTPIP" | sed s/\"//g)
 FINALLIST="$PIP/32,$CURRENTPIP"
-
-
 
 declare -A words
 
 IFS=","
 for w in $FINALLIST; do
-  #echo $w
-  #echo ""
   words+=( [$w]="" )
 done
 
-FLIST=$(echo \"${!words[@]}\" | sed 's/ /,/g')
-#echo ">>>>>"
-FL=$(echo $FLIST | sed 's/ /,/g' | sed 's/"//g')
-
-#echo ">>>>>"
-#echo $FL
-
-
+FLIST=$(echo ${!words[@]} | sed 's/ /,/g')
 
 echo "Set Empty List"
 az aks update \
@@ -44,8 +25,4 @@ az aks update \
   --api-server-authorized-ip-ranges ""
 
 echo "Update Allowed List"
-az aks update \
-  --resource-group $AKS_RG_NAME \
-  --name $AKS_NAME \
-  --api-server-authorized-ip-ranges $FL \
-  --debug
+echo "az aks update --resource-group $AKS_RG_NAME --name $AKS_NAME --api-server-authorized-ip-ranges $FLIST --debug" | bash 
