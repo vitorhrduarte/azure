19b0dcff90c0f3059c6227c76388cbaa3d8e64b2
Ubuntu <gits@ljs.gtbgjbosemxetduyonuoo02fdf.ax.internal.cloudapp.net>
2022-08-24 07:26:42 +0000

    Add logic to the code

--- az-cli/aks/authorized-ip-ranges/go.sh
+++ az-cli/aks/authorized-ip-ranges/go.sh
@@ -8,10 +8,10 @@ PIP=$(curl -s -4 ifconfig.io)
 
 CURRENTPIP=$(az aks show --resource-group $AKS_RG_NAME --name $AKS_NAME --query apiServerAccessProfile.authorizedIpRanges | jq -r ". | @csv")
 
-echo ""
-echo "PIP: $PIP"
-echo ""
-echo "CURRENT IP: $CURRENTPIP"
+#echo ""
+#echo "PIP: $PIP"
+#echo ""
+#echo "CURRENT IP: $CURRENTPIP"
 
 CURRENTPIP=$(echo "$CURRENTPIP" | sed s/\"//g)
 
@@ -19,10 +19,33 @@ FINALLIST="$PIP/32,$CURRENTPIP"
 
 
 
-echo ""
-echo "Final List: $FINALLIST"
+declare -A words
 
+IFS=","
+for w in $FINALLIST; do
+  #echo $w
+  #echo ""
+  words+=( [$w]="" )
+done
+
+FLIST=$(echo \"${!words[@]}\" | sed 's/ /,/g')
+#echo ">>>>>"
+FL=$(echo $FLIST | sed 's/ /,/g' | sed 's/"//g')
+
+#echo ">>>>>"
+#echo $FL
+
+
+
+echo "Set Empty List"
 az aks update \
   --resource-group $AKS_RG_NAME \
   --name $AKS_NAME \
-  --api-server-authorized-ip-ranges $FINALLIST
+  --api-server-authorized-ip-ranges ""
+
+echo "Update Allowed List"
+az aks update \
+  --resource-group $AKS_RG_NAME \
+  --name $AKS_NAME \
+  --api-server-authorized-ip-ranges $FL \
+  --debug
