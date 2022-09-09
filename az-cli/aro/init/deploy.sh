778eac7464baddfb5cfc56a3d913e058e2b1d9db
Vitor <vitorhrd@gmail.com>
2022-09-09 16:02:06 +0000

    Improve deployment file

--- az-cli/aro/init/deploy.sh
+++ az-cli/aro/init/deploy.sh
@@ -16,19 +16,22 @@ ARO_VNET_SUBNET_WORKER_NAME="snet-w-$ARO_CLUSTER_NAME"                  # Name o
 ARO_VNET_SUBNET_WORKER_NAME_CIDR="$ARO_VNET_PREFIX.64.0/18"             # Worker Subnet Address Prefixes
 
 
-ARO_PULL_SECRET_PATH=$ARO_FULL_PATH_PULL_SECRET
+ARO_PULL_SECRET_PATH=$ARO_SECRET_PULL_PATH
 
 
-# Create ARO Cluster Prerequisites :
+## Create ARO Cluster Prerequisites :
+echo "Create ARO RG"
 az group create \
   --name $ARO_RG_NAME \
   --location $ARO_RG_LOCATION
 
+echo "Create ARO Vnet"
 az network vnet create \
   --resource-group $ARO_RG_NAME \
   --name $ARO_VNET_NAME \
   --address-prefixes $ARO_VNET_CIDR
 
+echo "Create ARO Subnet for Master nodes"
 az network vnet subnet create \
   --resource-group $ARO_RG_NAME \
   --vnet-name $ARO_VNET_NAME \
@@ -36,6 +39,7 @@ az network vnet subnet create \
   --address-prefixes $ARO_VNET_SUBNET_MASTER_NAME_CIDR \
   --service-endpoints Microsoft.ContainerRegistry
 
+echo "Create ARO Subnet for Worker nodes"
 az network vnet subnet create \
   --resource-group $ARO_RG_NAME \
   --vnet-name $ARO_VNET_NAME \
@@ -43,12 +47,14 @@ az network vnet subnet create \
   --address-prefixes $ARO_VNET_SUBNET_WORKER_NAME_CIDR \
   --service-endpoints Microsoft.ContainerRegistry
 
+echo "Update ARO Subnet for Master nodes"
 az network vnet subnet update \
   --name $ARO_VNET_SUBNET_MASTER_NAME \
   --resource-group $ARO_RG_NAME \
   --vnet-name $ARO_VNET_NAME \
   --disable-private-link-service-network-policies true
 
+echo "Create ARO Cluster"
 az aro create \
   --resource-group $ARO_RG_NAME  \
   --name $ARO_CLUSTER_NAME \
@@ -57,13 +63,17 @@ az aro create \
   --worker-subnet $ARO_VNET_SUBNET_WORKER_NAME \
   --pull-secret $ARO_PULL_SECRET_PATH \
   --cluster-resource-group $ARO_NODE_RG_NAME \
-  --client-id $ARO_CLIENT_ID \
-  --client-secret $ARP_CLIENT_SECRET \
+  --client-id $ARO_SP_ID \
+  --client-secret $ARO_SP_SECRET \
   --debug
 
+echo "Get ARO's in SUB"
 az aro list \
   --output table
 
+echo "Get ARO Credentials"
 az aro list-credentials \
   --name $ARO_CLUSTER_NAME \
   --resource-group $ARO_RG_NAME
+
+
